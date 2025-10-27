d3a2b1bbb32859c5fe1e41708d88c2cf8c2b8c06
konstantin <konstantinefimov3742@gmail.com>
2025-10-27 18:24:33 +0700

    Input field

--- app/src/main/java/com/example/tipcalc/MainActivity.kt
+++ app/src/main/java/com/example/tipcalc/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.tipcalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tipcalc.ui.theme.TipCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipCalculatorApp() {
-    Text(text = "Калькулятор чаевых")
+    var orderAmount by remember { mutableStateOf("") }
+    var dishCount by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(text = "Сумма заказа: $orderAmount")
+        Text(text = "Количество блюд: $dishCount")
+    }
 }
 
 @Preview(showBackground = true)
